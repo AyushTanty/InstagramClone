245b0d3f01461584633c86c16a462ca93b876aa7
ASUS <ayushtanty003@gmail.com>
2024-07-25 17:06:36 +0530

    fixing error

--- app/src/main/java/com/example/instagramclone/SignUpActivity.kt
+++ app/src/main/java/com/example/instagramclone/SignUpActivity.kt
@@ -1,6 +1,7 @@
 package com.example.instagramclone
 
 import android.os.Bundle
+import android.text.Html
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
@@ -37,6 +38,8 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        val text = "<font color=#FF000000>Already have an Account</font> <font color=#1E88E5>Login ?</font>"
+        binding.login.setText(Html.fromHtml(text))
         user = User()
         binding.signUpBtn.setOnClickListener {
             if (binding.name.editText?.text.toString().equals("") or
